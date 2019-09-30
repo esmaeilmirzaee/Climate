4fa1e5a2a36334512dada5efb406a192238a6862
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2019-09-30 18:03:37 +0330

    init

---
+++ Climate/Controller/ChangeCityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChangeCityViewController.swift
+//  Climate
+//
+//  Created by Esmaeil MIRZAEE on 2019-09-30.
+//  Copyright © 2019 Esmaeil MIRZAEE. All rights reserved.
+//
+
+
+import UIKit
+
+class ChangeCityViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ Climate/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Climate
+//
+//  Created by Esmaeil MIRZAEE on 2019-09-30.
+//  Copyright © 2019 Esmaeil MIRZAEE. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
